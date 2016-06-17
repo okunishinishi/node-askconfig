e0324971c72f541b279fa9d79af8c2c2b350da2d
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-17 14:16:51 +0900

    Version incremented to 3.0.2

--- example/asking_shell_example.js
+++ example/asking_shell_example.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 'use strict'
 
-const askconfig = require('askconfig');
+const askconfig = require('askconfig')
 
 askconfig({
-    name: 'default-name',
-    description: ''
-}, (err, result) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    } else {
-        console.log("Result:", result);
-        process.exit(0);
-    }
-});
+  name: 'default-name',
+  description: ''
+}).then((result) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Result: ${result}`)
+    process.exit(0)
+  }
+})
 
